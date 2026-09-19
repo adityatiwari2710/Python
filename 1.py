e67a64f7b028d14f85cae60bf7cb7867d39da33c
Aditya Tiwari <aditya2710tiwari@gmail.com>
2026-09-19 22:30:53 +0530

    1

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 name = "Aditya Tiwari"
 age = 18
-gender = "male"
-print("my name is ;", name)
-print(age)
-print(gender)
+gender = "Male"
+print("My name is:",name)
+print("Age:", age)
+print("Gender:", gender)
