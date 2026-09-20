cbda9a3319dd5f46897c548ceaf3f311f56ef00c
Aditya Tiwari <aditya2710tiwari@gmail.com>
2026-09-20 16:26:13 +0530

    4

---
+++ 4.py
@@ -0,0 +1,7 @@
+print("Hello World")
+# my name is chat gpt
+# Single line comment ke liye use hota h hash
+print(" My name is Aditya Tiwari")
+'''
+multiple line comment ke liye use hota h triple (single or double) quote
+'''
